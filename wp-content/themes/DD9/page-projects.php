1f9c7eff05d52c475f2fe6ffa1e277e662d12e5e
Hilary Clarcq <hilary@dd9.com>
2013-04-23 16:20:58 -0600

    #/projects prepped for Taavo to be grouped by year

--- wp-content/themes/DD9/page-projects.php
+++ wp-content/themes/DD9/page-projects.php
@@ -34,166 +34,148 @@ $wd_services = get_posts(array(
 
 ?>
 
-            <div class="block_container full_width clearfix">
-              <div id="secondary">
-                <div class="two_column">
-                  <h4 class="subheading_full_width"><span>Work</span></h4>
-                    <div class="block_content">
-                      
-                      <p class="top_line"></p>
-                      
-                      <?php the_content(); ?>
-                    </div>
-                </div><!-- .two_column -->
-                
-                <div class="two_column">   
-                  <h4 class="subheading breadcrumbs">Services</a></h4>     
-                   <div class="block_content border_top">
-                   
-                   	 <p class="top_line">Below is a list of the graphic and web design services that we provide. Visit a page below to get in depth information about our design offerings.</p>
-                     
-                     <div id="services_container" class="sidebar">
+			<div class="block_container full_width clearfix">
+      	<div id="secondary">
+          <div class="two_column">
+            <h4 class="subheading_full_width"><span>Work</span></h4>
+          </div><!-- .two_column -->
+        </div><!-- #secondary -->
+          
+        <div class="content_right">
+          <article class="post plain">    
+            <div class="entry_content">   
+              <?php the_content(); ?>
+              <p>Below is a list of the graphic and web design services that we provide. Visit a page below to get in depth information about our design offerings.</p>
+            </div>                  
+            
+            <div id="services_container" class="project_index">
+              <?php if($wd_services): ?>
+              <ul class="services web_design clearfix">
+                <li class="parent_service subheading">
+                  <a href="<?= get_permalink($website_design->ID) ?>"><?= $website_design->post_title ?></a>
+                </li>
+                <?php foreach($wd_services as $service): ?>
+                  <li>
+                    <a href="<?= get_permalink($service->ID) ?>"><?= $service->post_title ?></a>
+                  </li>
+                <?php endforeach; ?>
+              </ul><!-- .services -->	
+              <?php else: ?>
+                  No services found.
+              <?php endif; ?>
+              
+              <?php if($wd_services): ?>
+              <ul class="services graphic_design clearfix">
+                <li class="parent_service subheading">
+                  <a href="<?= get_permalink($graphic_design->ID) ?>"><?= $graphic_design->post_title ?></a>
+                </li>
+                <?php foreach($gd_services as $service): ?>
+                  <li>
+                    <a href="<?= get_permalink($service->ID) ?>"><?= $service->post_title ?></a>
+                  </li>
+                <?php endforeach; ?>
+              </ul><!-- .services -->	
+              
+              <?php else: ?>
+                  No services found.
+              <?php endif; ?>
+            
+            </div><!-- services_container -->
+          </article>
+        </div><!-- .content_right thin_border -->
+      </div><!-- .block_container.full_width --> 
+          
+			
       
-                      <?php if($wd_services): ?>
-                      <ul class="services web_design clearfix">
-                        <li class="parent_service subheading">
-                          <a href="<?= get_permalink($website_design->ID) ?>"><?= $website_design->post_title ?></a>
-                        </li>
-                        <?php foreach($wd_services as $service): ?>
-                          <li>
-                            <a href="<?= get_permalink($service->ID) ?>"><?= $service->post_title ?></a>
-                          </li>
-                        <?php endforeach; ?>
-                      </ul><!-- .services -->	
-                      <?php else: ?>
-                          No services found.
-                      <?php endif; ?>
-                      
-                      <?php if($wd_services): ?>
-                      <ul class="services graphic_design clearfix">
-                        <li class="parent_service subheading">
-                          <a href="<?= get_permalink($graphic_design->ID) ?>"><?= $graphic_design->post_title ?></a>
-                        </li>
-                        <?php foreach($gd_services as $service): ?>
-                          <li>
-                            <a href="<?= get_permalink($service->ID) ?>"><?= $service->post_title ?></a>
-                          </li>
-                        <?php endforeach; ?>
-                      </ul><!-- .services -->	
-                      
-                      <?php else: ?>
-                          No services found.
-                      <?php endif; ?>
-                    
-                    </div><!-- services_container -->
-                   </div><!-- .block_content -->
-                
-                </div><!-- .two_column -->
+      <?php 
+			// We'd like to group the projects by their "End Work" year, most recent first.
+			// For each year, load the year in the left column and the thumbmail grid of projects in the right content area.
+			?>
+      <div class="block_container full_width clearfix"> 
+        <div class="two_column">
+        	
+          <?php // Get the year ?>
+          <h4 class="subheading breadcrumbs alt_xl">2013</h4>
+          
+          <div class="block_content border_top"> </div>
+        </div><!-- .two_column --> 
+        
+        <div class="content_right">   
+          
+          <?php // Get the projects ?>
+          <?php if($projects): ?>
               
-              </div><!-- #secondary -->
+            <ul id="thumbnail_grid" class="clearfix">
               
-              <div class="content_right">   
-               
-                <?php if($projects): ?>
-                
-                	<?php if($wd_services): ?>
-                      <ul class="filters">
-                      <!-- Add the commenting between each <li> to remove the automatic inline block spacing: easier than doing yet another floated element -->
-                        
-                        <li><a href="#" data-filter=".all" class="instruction">Filter by:</a> </li><!--
-  						--><li>
-                        <a href="#" data-filter=".all">All</a></li><!--
-  						--><li>
-                          <a href="#" data-filter=".<?= $website_design->post_name ?>"><?= $website_design->post_title ?></a>
-                        </li><!--
-  						--><?php foreach($wd_services as $service): ?><li>
-                          <a href="#" data-filter=".<?= $service->post_name ?>"><?= $service->post_title ?></a>
-                        </li><!--
-  						--><?php endforeach; ?><li>
-                          <a href="#" data-filter=".<?= $graphic_design->post_name ?>"><?= $graphic_design->post_title ?></a>
-                        </li><!--
-  						--><?php foreach($gd_services as $service): ?><li>
-                           <a href="#" data-filter=".<?= $service->post_name ?>"><?= $service->post_title ?></a>
-                          </li><!--
-  						--><?php endforeach; ?>
-                        
-                      </ul><!-- .services -->	
-                      <?php else: ?>
-                          No services found.
-                      <?php endif; ?>
-                      
-                    <ul id="thumbnail_grid" class="clearfix">
-                    	<?php foreach($projects as $project): ?>
-                        	<?php 
+							<?php foreach($projects as $project):
 							
 							$project_services = get_posts(array(
-								'connected_type' => 'project_services',
-								'post_type' => 'service',
-								'suppress_filters' => false,
-								'numberposts' => -1,
-								'connected_from' => $project->ID,
-								'connected_orderby' => '_order_to'
+							'connected_type' => 'project_services',
+							'post_type' => 'service',
+							'suppress_filters' => false,
+							'numberposts' => -1,
+							'connected_from' => $project->ID,
+							'connected_orderby' => '_order_to'
 							));
+	
+							$images = get_posts(array(
+							'numberposts' => 1,
+							'order'=> 'ASC',
+							'orderby' => 'menu_order',
+							'post_mime_type' => 'image',
+							'post_parent' => $project->ID,
+							'post_status' => null,
+							'post_type' => 'attachment'
+							)); 
 							
-                        	$images = get_posts(array(
-                                'numberposts' => 1,
-                                'order'=> 'ASC',
-                                'orderby' => 'menu_order',
-                                'post_mime_type' => 'image',
-                                'post_parent' => $project->ID,
-                                'post_status' => null,
-                                'post_type' => 'attachment'
-                            )); 
-                            
-                            if($images)
-                            {
-                                $image_data = wp_get_attachment_image_src($images[0]->ID, 'thumbnail');
-                                $image_src = $image_data[0];
-                            }
-                            
-                            $attributes = wp_get_post_terms($project->ID, 'attribute');
-                        	?>          
-                            <li class="element all<?php foreach($project_services as $project_service){ echo " ". $project_service->post_name; }?>">
-                                <div class="preview_thumbnail client">
-                                    <?php if($images): ?>
-                                        <a href="<?= get_permalink($project->ID) ?>" title="<?= $project->post_title ?>">   
-                                            <img src="<?= $image_src ?>" width="234" height="162" alt="<?php $project->post_title; ?> Preview" />
-                                        </a>
-                                    <?php else: ?>
-                                        <a href="<?= get_permalink($project->ID) ?>" title="<?= $project->post_title ?>">
-                                            <img src="http://dd9.com/wp-content/uploads/feat_placeholder.jpg" alt="<?php $project->post_title; ?> Preview" width="234" height="162" />
-                                        </a>
-                                    <?php endif; ?>                                
-                                </div>
-                            	
-                                <a href="<?= get_permalink($project->ID) ?>" class="info_panel">
-                                    <span class="thumbnail_title"><?= $project->post_title ?></span>
-         
-                                    <?php if($attributes): $i = 0; ?>                                  
-                                        <ul class="thumbnail_tags">                                        
-                                            <?php foreach($attributes as $attribute): if($i == 6) break; ?>
-                                                <li>
-                                                    <?= $attribute->name ?><?php if($i != count($attributes) - 1) echo "," ?> 
-                                                </li>
-                                            <?php $i++; endforeach; ?>
-                                        </ul><!-- .project_tags -->
-                                    <?php endif; ?>
-                                </a><!-- #info_panel -->
-                            
-                            
-                            </li>	
-                        <?php endforeach; ?>    
+							if($images)
+							{
+								$image_data = wp_get_attachment_image_src($images[0]->ID, 'thumbnail');
+								$image_src = $image_data[0];
+							}
+							
+							$attributes = wp_get_post_terms($project->ID, 'attribute');
+							?>          
+              <li class="element all<?php foreach($project_services as $project_service){ echo " ". $project_service->post_name; }?>">
+                <div class="preview_thumbnail client">
+                  <?php if($images): ?>
+                    <a href="<?= get_permalink($project->ID) ?>" title="<?= $project->post_title ?>">   
+                      <img src="<?= $image_src ?>" width="234" height="162" alt="<?php $project->post_title; ?> Preview" />
+                    </a>
+                  <?php else: ?>
+                    <a href="<?= get_permalink($project->ID) ?>" title="<?= $project->post_title ?>">
+                      <img src="http://dd9.com/wp-content/uploads/feat_placeholder.jpg" alt="<?php $project->post_title; ?> Preview" width="234" height="162" />
+                    </a>
+                  <?php endif; ?>                                
+                </div>
                 
-    				</ul><!-- #thumbnail_grid -->	
-				<?php else: ?>
-                    No projects found.
-                <?php endif; ?>
+                <a href="<?= get_permalink($project->ID) ?>" class="info_panel">
+                  <span class="thumbnail_title"><?= $project->post_title ?></span>
 
-                <footer class="entry-meta">
-					<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
-                </footer><!-- .entry-meta -->
+                  <?php if($attributes): $i = 0; ?>                                  
+                    <ul class="thumbnail_tags">                                        
+                      <?php foreach($attributes as $attribute): if($i == 6) break; ?>
+                        <li>
+                          <?= $attribute->name ?><?php if($i != count($attributes) - 1) echo "," ?> 
+                        </li>
+                      <?php $i++; endforeach; ?>
+                    </ul><!-- .project_tags -->
+                  <?php endif; ?>
+                </a><!-- #info_panel -->
+              </li>	
               
-              </div><!-- .content_right -->   
+              <?php endforeach; ?>    
+          
+          	</ul><!-- #thumbnail_grid -->	
+          <?php else: ?>
+            No projects found.
+          <?php endif; ?>
+  
+        <footer class="entry-meta">
+          <?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+        </footer><!-- .entry-meta -->
+              
+      	</div><!-- .content_right -->   
 			</div><!-- .block_container.full_width --> 
-            
+          
 <?php get_footer(); ?>
