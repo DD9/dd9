1e02bcb98922adeaf0deb0e3236fd858e12f1473
Todd Dumas <todd@dd9.com>
2013-04-25 10:59:03 -0600

    minor fix to footer to say i contributed to this round

--- wp-content/themes/DD9/footer.php
+++ wp-content/themes/DD9/footer.php
@@ -67,7 +67,7 @@
 			?>
 
 			<div id="site-generator">
-				<p>Copyright &copy; 2001 - 2012 DD9, Inc. All rights reserved. <a href="/wp-admin/" style="color:#CCC; font-size:8px; float:right" rel="nofollow">Admin Login</a></p>
+				<p>Copyright &copy; 2001 - <?php echo date('Y'); ?>  DD9, Inc. All rights reserved. <a href="/wp-admin/" style="color:#CCC; font-size:8px; float:right" rel="nofollow">Admin Login</a></p>
 
 <div itemscope itemtype="http://data-vocabulary.org/Organization" id="microformat_block"> 
     <span itemprop="name">DD9</span>, Inc.  &#8226; 
